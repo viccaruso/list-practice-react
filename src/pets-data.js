50ba6915b843be089511abe4e0158b4ee1078fee
Victor Caruso <vic.caruso@gmail.com>
2022-01-27 13:15:57 -0800

    set up pets-data.js export

---
+++ src/pets-data.js
@@ -0,0 +1,41 @@
+export const pets = [
+  {
+    name: 'Pepper',
+    type: 'Dog',
+    favoriteToys: [
+      'Squeaky Pig',
+      'Nylabone',
+      'Sticks'
+    ],
+    color: 'Black & White'
+  },
+  {
+    name: 'Kiko',
+    type: 'Dog',
+    favoriteToys: [
+      'Orange ball',
+      'Nylabone',
+      'Fuzzy slippers'
+    ],
+    color: 'Black & White'
+  },
+  {
+    name: 'Coco',
+    type: 'Dog',
+    favoriteToys: [
+      'Kong',
+      'Chuck-it'
+    ],
+    color: 'Brown & White'
+  },
+  {
+    name: 'Charlie',
+    type: 'Cat',
+    favoriteToys: [
+      'Ball of yarn',
+      'Anything on the edge of a counter',
+      'Catnip'
+    ],
+    color: 'Gray stripes'
+  }
+];
