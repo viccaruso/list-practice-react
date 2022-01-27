da1ece202408d4dee935dd37872599de859ad2ee
Victor Caruso <vic.caruso@gmail.com>
2022-01-27 13:40:21 -0800

    set up books data export

---
+++ src/books-data.js
@@ -0,0 +1,47 @@
+export const books = [
+  {
+    ISBN: '9780553588484',
+    title: 'A Game of Thrones',
+    author: {
+      firstName: 'George',
+      lastName: 'Martin',
+      middleName: 'R.R.'
+    },
+  },
+  {
+    ISBN: '9780756404079',
+    title: 'The Name of the Wind',
+    author: {
+      firstName: 'Patrick',
+      lastName: 'Rothfuss',
+      middleName: ''
+    },
+  },
+  {
+    ISBN: '9781841499888',
+    title: 'Leviathan Wakes',
+    author: {
+      firstName: 'James',
+      lastName: 'Corey',
+      middleName: 'S.A.'
+    },
+  },
+  {
+    ISBN: '9780593099322',
+    title: 'Dune',
+    author: {
+      firstName: 'Frank',
+      lastName: 'Herbert',
+      middleName: ''
+    },
+  },
+  {
+    ISBN: '9780441569595',
+    title: 'Neuromancer',
+    author: {
+      firstName: 'William',
+      lastName: 'Gibson',
+      middleName: ''
+    },
+  },
+];
